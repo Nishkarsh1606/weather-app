76ba4e8cfa405cfe1ae4cde8c8ea14fbc5d49c47
NishkarshS-iMac <ns513@student.london.ac.uk>
2022-09-17 19:12:09 +0530

    segment code into get and set

--- script.js
+++ script.js
@@ -6,28 +6,26 @@ const currentTemp=document.querySelector('#currentTemp')
 const maxTemp=document.querySelector('#maxTemp')
 const minTemp=document.querySelector("#minTemp")
 
-const getWeather=async()=>{
-    try {
-        const searchCityName=searchBarInput.value
-        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCityName}&appid=a6d8d3e278a1745de95c8de12eb58ce2&units=imperial`)
-        const data=await response.json()
-        console.log(data);
-        cityName.innerHTML=data.name
-        weatherStat.innerHTML=data.weather[0].main //for message- eg: clouds
-        currentTemp.innerHTML=`<span>Temp: ${data.main.temp}<span>`
-        maxTemp.innerHTML=`<span>Max-Temp: ${data.main.temp_max}</span>`
-        minTemp.innerHTML=`<span>Min-Temp: ${data.main.temp_min}</span>`   
-    } catch (error) {
-        console.log(error);
-        alert('Error occured.Please reload the page.')
-    }
+
+const getWeatherData=async()=>{
+    const searchCityName=searchBarInput.value
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCityName}&appid=a6d8d3e278a1745de95c8de12eb58ce2`)
+    const data=await response.json()
+    setWeatherData(data)
+    console.log(data);
 }
 
-searchBtn.addEventListener('click',getWeather)
-document.addEventListener('keypress',function(e){
-if(e.key==='Enter'){
-    getWeather()
+const setWeatherData=(data)=>{
+    cityName.innerHTML=data.name
+    weatherStat.innerHTML=data.weather[0].main //for message- eg: clouds
+    currentTemp.innerHTML=`<span>Temp: ${data.main.temp}<span>`
+    maxTemp.innerHTML=`<span>Max-Temp: ${data.main.temp_max}</span>`
+    minTemp.innerHTML=`<span>Min-Temp: ${data.main.temp_min}</span>`
+}
+
+searchBtn.addEventListener('click',getWeatherData)
+document.addEventListener('keypress',(e)=>{
+    if(e.code==='Enter'){
+        getWeatherData()
     }
 })
-
-//Now divide the code into fetch-->get-->display modules
